535a5584df604e5b20c8752786914957c34f6253
William <lucyeni.we@gmail.com>
2019-05-13 21:01:53 +0100

    [UPDATE]
    Added an option for Login with different details
    -
    User can now change details on login page
    -

--- src/unilagwifi.user.js
+++ src/unilagwifi.user.js
@@ -1,8 +1,5 @@
-if(window.location.href=='http://hsunilaggw.unilag.edu.ng/login?dst=http%3A%2F%2Fwww.msftconnecttest.com%2Fredirect' ||
-        window.location.href=='http://guapgw.unilag.edu.ng/login' ||
-        window.location.href== 'http://guapgw.unilag.edu.ng/login?dst=http%3A%2F%2Fwww.msftconnecttest.com%2Fredirect'){
-            if(localStorage.length==0){
-                alert('You Have to be a student of UNILAG Since at least 2017. Just a Heads up ;)');
+ function manualSignIn(){
+    alert('You Have to be a student of UNILAG Since at least 2017. Just a Heads up ;)');
                 let firstName=prompt('Type in your First Name (It Has to be your first name, so it works)');
                 let matNo=prompt('Type in your Matric Number');
                 let uName= `${matNo}@students.unilag.edu.ng`;
@@ -11,10 +8,23 @@ if(window.location.href=='http://hsunilaggw.unilag.edu.ng/login?dst=http%3A%2F%2
                 localStorage.setItem('password',`${password}`);
                     document.getElementsByName('username')[1].value=uName;
                     document.getElementsByName('password')[1].value=password;
+ }
+ function autoSignIn(){
+    document.getElementsByName('username')[1].value=localStorage.userName;
+    document.getElementsByName('password')[1].value=localStorage.password;
+ }
+ if(window.location.href=='http://hsunilaggw.unilag.edu.ng/login'||
+    window.location.href=='http://guapgw.unilag.edu.ng/login'){  
+        if(localStorage.length==0){
+                manualSignIn();
             }
-            else{
-                document.getElementsByName('username')[1].value=localStorage.userName;
-                document.getElementsByName('password')[1].value=localStorage.password;
+        else{
+                if(confirm('Do You Want To Login With New Details?')==true){
+                    localStorage.clear();
+                    manualSignIn();
+                }else{
+                    autoSignIn();
+                }   
             }   
     }
 document.querySelector('input[type=submit]').click();
